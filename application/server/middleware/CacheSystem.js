d59132688afb91a7c6e5908f56c98264188fa1d4
freshskates <business.robertcacho@gmail.com>
2021-06-04 22:13:56 -0700

    Finish Cache System

--- application/server/middleware/CacheSystem.js
+++ application/server/middleware/CacheSystem.js
@@ -1,11 +1,17 @@
-const redisPool = require("./redisPool");
+const redisPool = require("../config/redisPool");
 
 const CacheSystem = {};
 
 CacheSystem.allMovies = async (req, res, next) => {
-  const reply = await redisPool.getAsync("allMovies");
-  if (reply) res.send(reply);
-  else next();
+  try {
+    const reply = await redisPool.getAsync("allMovies");
+    console.log("Status: allMovies Cached Call");
+    if (reply) res.send(JSON.parse(reply));
+    else next();
+  } catch (err) {
+    console.log(err);
+    next();
+  }
 };
 
 module.exports = CacheSystem;
--- application/server/routes/api/movies.js
+++ application/server/routes/api/movies.js
@@ -4,7 +4,9 @@ const express = require("express");
 const router = express.Router();
 const Movies = require("../../Models/Movies");
 const Cache = require("../../helper/Cache");
+const CacheSystem = require("../../middleware/CacheSystem");
 let count = 0;
+
 router.get("/", async (req, res) => {
   let cache = await Cache.get("test");
 
@@ -17,12 +19,11 @@ router.get("/", async (req, res) => {
   res.send({ key: await Cache.get("test") });
 });
 
-router.get("/getLatest/:page(\\d)", async (req, res) => {
+router.get("/getLatest/:page(\\d)", CacheSystem.allMovies, async (req, res) => {
   console.log(`Status: getLatest(${req.params.page})`);
-
   try {
     let results = await Movies.getLatest(req.params.page);
-    console.log(results);
+    await Cache.set("allMovies", JSON.stringify(results), 15);
     res.send(results);
   } catch (err) {
     res.send({ error: "Error Retrieving Movie Information" });
