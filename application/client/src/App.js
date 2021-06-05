0845429efbcf60e355ee410c6083a0478bbe3d1e
freshskates <business.robertcacho@gmail.com>
2021-06-04 19:21:07 -0700

    Add Front end Rendering

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,13 +1,30 @@
 import "./App.css";
 import Row from "./components/Row";
+import Movie from "./components/Movie";
 import requests from "./controller/requests";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      <Row fetchURL={requests.fetchLatest} />
-      <Row fetchURL={requests.fetchLatest} />
-      <Row fetchURL={requests.fetchLatest} />
-    </div>
+    <Router>
+      <Route path="/movie/:id">
+        <Movie />
+      </Route>
+      <Route exact path="/">
+        <div className="App">
+          <Row fetchURL={requests.fetchLatest} />
+          <Row fetchURL={requests.fetchLatest} />
+          <Row fetchURL={requests.fetchLatest} />
+        </div>
+      </Route>
+    </Router>
   );
 }
 
--- application/client/src/components/Row.js
+++ application/client/src/components/Row.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "../controller/axios";
+import { Link } from "react-router-dom";
+
 import "./Row.css";
 const baseURL = "https://image.tmdb.org/t/p/original/";
 
@@ -7,20 +9,21 @@ function Row({ title, fetchURL, isLargeRow }) {
   const [movies, setMovies] = useState([]);
 
   const renderMovies = (movies) => {
-
     return movies.map((movie) => {
       return (
-        <div className="container" onClick={}>
-          <img
-            key={movie.id}
-            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-            src={`${baseURL}${
-              isLargeRow ? movie.poster_path : movie.backdrop_path
-            }`}
-            alt={movie.name}
-          />
-          <a className="">{movie.title}</a>
-        </div>
+        <Link to={`/movie/${movie.id}`}>
+          <span className="container">
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${baseURL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+            <p>{movie.title}</p>
+          </span>
+        </Link>
       );
     });
   };
