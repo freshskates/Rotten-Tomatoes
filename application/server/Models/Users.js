77b15846e6e187cbf14c2ae6ce61f313c86e8767
freshskates <business.robertcacho@gmail.com>
2021-06-12 07:10:41 -0700

    :memo: Add Server side dependencies

---
+++ application/server/Models/Users.js
@@ -0,0 +1,6 @@
+const bcrypt = require("bcrypt");
+const db = require("bcrypt");
+
+const Users = {};
+
+module.exports = Users;
--- application/server/routes/api/auth.js
+++ application/server/routes/api/auth.js
@@ -1,8 +1,20 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/user", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/register", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.post("/validate", function (req, res, next) {
   res.send("respond with a resource");
 });
 
