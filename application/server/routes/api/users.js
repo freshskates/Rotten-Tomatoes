b45abc3c09daa22f8861489d3d26d30c1a42027a
freshskates <business.robertcacho@gmail.com>
2021-06-24 00:22:17 -0700

    Add User Endpoints

--- application/server/routes/api/users.js
+++ application/server/routes/api/users.js
@@ -5,5 +5,17 @@ const router = express.Router();
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+
+router.get("/authenticate", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/validate", function (req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/register", function (req, res, next) {
+  res.send("respond with a resource");
+});
 // //
 module.exports = router;
